8aa7bc305aa25bfa253d034c7c79458d65fda4c9
gregorczykwiktor <gregorczyk.wiktor01@gmail.com>
2019-04-16 15:20:07 +0200

    test

---
+++ 4_instrukcje_warunkowe.py
@@ -0,0 +1,47 @@
+# teletype wtyczka kozak
+
+x = 5
+
+if x == 5:
+    print('x jest rowne 5')
+else:
+    print('x nie jest rowne 5')
+
+############################################
+
+y = True #False
+
+if y:
+    print('Prawda')
+else:
+    print("Falsz")
+
+############################################
+
+z = 5 > 6
+
+if z:
+    print('Prawda')
+else:
+    print("Falsz")
+
+############################################
+
+# j = '1'
+# j = '0'
+# j = ' '
+# j = Falsz
+
+if bool(j):
+    print(1)
+else:
+
+############################################
+
+k = input("Podaj k: ")
+
+if bool(k):
+    print('Zmienna k zawiera dane.')
+else:
+    print('Zmienna k nie zawiera danych.')
+
