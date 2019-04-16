21ef7c154bb25ed6d995ff93bb5f91abf3d842fb
gregorczykwiktor <gregorczyk.wiktor01@gmail.com>
2019-04-16 16:10:54 +0200

    test

---
+++ 7_slowniki.py
@@ -0,0 +1,24 @@
+slownik = {'klucz1':'wartosc1','klucz2':'wartosc2'}
+
+# Utworz sklownik o nazwie pracownik zawierajacy klucze:
+# imie, nazwisko, miasto, wiek, imionaDzieci, imionaRodzicow
+
+pracownik ={
+    'imie':'',
+    'nazwisko':'',
+    'miasto':'',
+    'wiek':'',
+    'imionaDzieci':[],
+    'imionaRodzicow':[]
+}
+
+pracownik['imie']='jaroslaw'
+pracownik['nazwisko']='smorawa'
+pracownik['miasto']='gdynia'
+pracownik['wiek']=42
+pracownik['imionaDzieci'].append('Mariusz')
+pracownik['imionaDzieci'].append('Daniel')
+pracownik['imionaRodzicow'].append('Mariola')
+pracownik['imionaRodzicow'].append('Antioni')
+
+print(pracownik)
