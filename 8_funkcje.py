550d415866f02329d2d0d913feab16db0f96a093
gregorczykwiktor <gregorczyk.wiktor01@gmail.com>
2019-04-16 16:42:49 +0200

    test

---
+++ 8_funkcje.py
@@ -0,0 +1,53 @@
+def witaj():
+    print('Witaj Janusz!')
+
+witaj()
+
+def wyswietlWiek(wiek):
+    print('Twoj wiek: ',wiek)
+
+wyswietlWiek(23)
+
+def iloczyn(a, b):
+    return a * b
+
+print(iloczyn(3, 4))
+
+def iloraz(a, b):
+    return a / b
+
+iloraz1 = iloraz(3, 4)
+print(iloraz1)
+print(type(iloraz1))
+
+print(iloraz(b=5, a=2))
+
+# Uzytkownik podaje z klawiatury:
+# marka, model, pojemnosc, predkoscMax
+# utowrz funkcje, ktora pobierze od uzytkownika i przypisze do slownika
+# utworz druga funkcje wyswietlajaca dane w formacie:
+# Marka:...
+# Model:...
+
+auto={
+    'marka':'',
+    'model':'',
+    'pojemnosc':'',
+    'predkoscMax':''
+}
+
+print(type(auto['marka']))
+
+def wprowadz():
+    auto['marka'] = input()
+    auto['model'] = input()
+    auto['pojemnosc'] = input()
+    auto['predkoscMax'] = input()
+
+wprowadz()
+
+print(auto)
+
+
+
+
