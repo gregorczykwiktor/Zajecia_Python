f1a872c3a968b3788a48af828b5f1625e7154edc
gregorczykwiktor <gregorczyk.wiktor01@gmail.com>
2019-04-16 15:36:01 +0200

    test

---
+++ 5_listy.py
@@ -0,0 +1,41 @@
+# [] - listy
+# () -
+# {} - slowniki
+
+programowanie = ['python','php','csharp','js','java']
+print(type(programowanie))
+
+pierwszy = programowanie[0]
+print(pierwszy)
+
+trzyElementy = programowanie[0:3]
+print(trzyElementy)
+
+ostatniElement = programowanie[-1]
+print(ostatniElement)
+
+# Dodanie nowego elementu na koniec listy
+
+programowanie.append('R')
+programowanie.append('Python')
+print(programowanie)
+
+#zliczanie elementow w liscie
+ile = programowanie.count('Python')
+print(ile)
+
+# ilosc elementow
+iloscElem = len(programowanie)
+print(iloscElem)
+
+# polaczenie list
+inneJezyki = ['c','c++']
+programowanie.extend(inneJezyki)
+print(programowanie)
+
+#czyszczenie list
+nowa = programowanie
+print('lista nowa: ',end="")
+print(nowa)
+nowa.clear()
+print('Lista nowa: ',end="")
