a66dbe4402042568f5e85f7b6f9cab8080da75ec
gregorczykwiktor <gregorczyk.wiktor01@gmail.com>
2019-04-16 16:20:48 +0200

    test

--- 7_slowniki.py
+++ 7_slowniki.py
@@ -27,3 +27,34 @@ pracownik['wzrost'] = 175
 pracownik['waga'] = 75
 
 print(pracownik)
+
+klucz = 'wiek'
+
+pracownik1 ={
+    'imie':'Anna',
+    'nazwisko':'Kazimierkowa',
+    'miasto':'Gdynia',
+    'wiek':'43',
+    'imionaDzieci':['Blazej','Martyna'],
+    'imionaRodzicow':['Magda','Janusz']
+}
+
+print(pracownik1)
+
+if klucz in pracownik1:
+    del pracownik1[klucz]
+    print(f'klucz {klucz} zostal usuniety')
+else:
+    print(f'klucz {klucz} nie zostal usuniety')
+
+print(pracownik1)
+print(pracownik1.keys())
+print(pracownik1.values())
+print(list(pracownik1.values()))
+print(pracownik1.items())
+
+for value in pracownik1.values():
+    print(value, end=' ')
+
+for key, value in pracownik1.items():
+    print(f'{key}:{value}')
