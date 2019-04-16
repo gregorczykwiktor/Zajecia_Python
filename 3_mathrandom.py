46547aa0dca732588d0dc5ab63add81935754c13
gregorczykwiktor <gregorczyk.wiktor01@gmail.com>
2019-04-16 15:02:41 +0200

    test

---
+++ 3_mathrandom.py
@@ -0,0 +1,33 @@
+import math
+# liczba pi
+pi = math.pi
+print(pi)
+
+# pierwiastek
+pierwiastek = math.sqrt(9)
+print(pierwiastek)
+
+# losowanie - modul random
+import random
+losuj = random.random()
+print(losuj)
+
+# losuj z listy
+losujZlisty = random.choice([1,2,33,4])
+print(losujZlisty)
+
+# instrukcje warunkowe
+
+
+
+# Uzytkownik podaje z klawiatury dwie wartosci.
+# Maksymalna i minimalna przedialu z ktorego bedzie wylosowane 5 liczb calkowitych.
+# Wylosowane liczby wyswietlij w formacie:
+# Liczba1: ....
+# Liczba2: ....
+# ....
+# Liczba5: ....
+
+
+# sublime block comment
+# atom python run
